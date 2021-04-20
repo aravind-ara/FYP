9f91c5682bb74621fc6688e48b068df5e6cfeef6
aravind-ara <asokanaravind7@gmail.com>
2021-04-20 16:45:23 +0530

    final

--- frontend/Main_script.js
+++ frontend/Main_script.js
@@ -392,7 +392,7 @@ function updateClock(){
     var months = ["January", "February", "March", "April", "May", "June", "July", "Augest", "September", "October", "November", "December"];
     var week = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
     var ids = ["dayname", "month", "daynum", "year", "hour", "minutes", "seconds", "period"];
-    var values = [week[dname], mo, dnum.pad(2), yr, hou.pad(2), min.pad(2), sec.pad(2), pe];
+    var values = [week[dname], mo+1, dnum.pad(2), yr, hou.pad(2), min.pad(2), sec.pad(2), pe];
     for(var i = 0; i < ids.length; i++)
     document.getElementById(ids[i]).firstChild.nodeValue = values[i];
 }
